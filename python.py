f889c41297050bd1b2560219718b085b97618f62
harshdeepsingh2005 <sabharwal1120@gmail.com>
2023-10-24 20:52:39 +0530

    minor edits

--- python.py
+++ python.py
@@ -1,15 +1,7 @@
-n = int(input("enter no. of students: "))
-a = []
-i = 0
-for i in range(n):
-    print("enter the age of student no.", i + 1, ":")
-    a[i] = int(input())
-big = a[0]
-small = a[0]
-for i in range(1, n):
-    if big > a[i]:
-        big = a[i]
-    if small < a[i]:
-        small = a[i]
-print("biggest=", big)
-print("smallest=", small)
+s= input("input: ")
+l=len(s)
+
+if l>3:
+    print("output:",s[1:-1])
+else:
+    print("output:",s)
